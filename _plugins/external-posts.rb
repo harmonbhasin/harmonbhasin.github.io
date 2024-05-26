91602cf36d0f2b8aeb26bd1673c214caeb16800f
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2024-05-26 12:41:50 -0500

    Maybe avoid cloudflair

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -12,7 +12,9 @@ module ExternalPosts
       if site.config['external_sources'] != nil
         site.config['external_sources'].each do |src|
           p "Fetching external posts from #{src['name']}:"
-          xml = HTTParty.get(src['rss_url']).body
+          xml = HTTParty.get(src['rss_url'], headers: {
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+          })).body
           return if xml.nil?
 
           p xml
