9f38ca5e077c8ef5615ae70fb6af4c43898436d2
Harmon Bhasin <harmonprograms@protonmail.com>
2025-02-23 17:21:15 -0500

    Updated to now host on vercel.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('mdsvex').MdsvexOptions} */
@@ -18,9 +18,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/harmonbhasin.github.io' : ''
-		}
 	},
 
     extensions: [".svelte", ".md"]
